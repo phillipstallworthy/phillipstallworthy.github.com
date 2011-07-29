848580f3ba2cb93b585ac59a11fb4089473e5aac
Phillip Stallworthy <phillipstallworthy@yahoo.com>
2011-07-29 17:41:14 +1200

    tileData required

---
+++ release/dojo/games/tileData.js
@@ -0,0 +1,8 @@
+/*
+	Copyright (c) 2004-2011, The Dojo Foundation All Rights Reserved.
+	Available via Academic Free License >= 2.1 OR the modified BSD license.
+	see: http://dojotoolkit.org/license for details
+*/
+
+
+if(!dojo._hasResource["games.tileData"]){dojo._hasResource["games.tileData"]=true;dojo.provide("games.tileData");games.tileData={top:0,left:1,bottom:2,right:3,moduleSize:undefined,logSize:function(_1){this.moduleSize=_1;console.log(this.moduleSize);}};}
